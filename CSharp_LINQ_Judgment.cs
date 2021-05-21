a83352fd93dc130c5ab7b2177ca981f363f80a86
影山榮一 <a1kageyama@kageyamBookpuro.AirPort>
2021-05-21 15:12:43 +0900

    Update

--- CSharp_LINQ_Judgment.cs
+++ CSharp_LINQ_Judgment.cs
@@ -4,44 +4,40 @@ using System.Collections.Generic;
 
 namespace Judge
 {
-	class MainClass 
-	{
-		public static void Main(string[] args)
-		{
-			var jud = new JudgeClass();
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var judgeClass = new JudgeClass();
 
-			jud.GetJudge();
-		}
-	}
+            judgeClass.GetJudge();
+        }
+    }
 
-	class JudgeClass
-	{
-		List<int> source = new List<int>
-		{
-			2, 4, 6, 8, 10, 12, 14, 16
-		};
+    class JudgeClass
+    {
+        List<int> source = new List<int>
+        {
+            2, 4, 6, 8, 10, 12, 14, 16
+        };
 
-		public void GetJudge()
-		{
-			// All()
-			// 全ての要素が、条件を満たしているか判定
-			Console.WriteLine(source.All(e => e >= 6));
+        public void GetJudge()
+        {
+            // All()
+            // Determine if all elements meet the conditions
+            Console.WriteLine(source.All(e => e >= 6));
 
-			// Any()
-			// 条件を満たす要素が含まれているか判定
-			Console.WriteLine(source.Any(e => e >= 14));
+            // Any()
+            // Determine whether elements that satisfy the conditions are included
+            Console.WriteLine(source.Any(e => e >= 14));
 
-			// Contains()
-			// 指定した要素が含まれているか判定
-			Console.WriteLine(source.Contains(11));
+            // Contains()
+            // Determine if the specified element is included
+            Console.WriteLine(source.Contains(11));
 
-			// SequenceEqual()
-			// 2つのシーケンスが等しいか判定
-			Console.WriteLine(source.SequenceEqual( new List<int>
-													{
-														2, 4, 6, 8, 10, 12, 14, 16
-													} ));
-		}
-	}
+            // SequenceEqual()
+            // Determine if two sequences are equal
+            Console.WriteLine(source.SequenceEqual(new List<int> { 2, 4, 6, 8, 10, 12, 14, 16}));
+        }
+    }
 }
-
